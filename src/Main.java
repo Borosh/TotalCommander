e1b0818963fe6b7a7591dd5c21d62945111151fd
unknown <TerhesBalazs>
2017-04-25 16:51:37 +0200

    Eclipse commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
     	System.out.println("Ha ezt latjatok, akkor sikerult a feltoltes!");
+    	System.out.println("Eclipseről feltöltve!");
     }
 }
