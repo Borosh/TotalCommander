5dde614bc81d47aae659c61224ae7adc80268660
Borosh <aboros97@gmail.com>
2017-04-29 19:08:06 +0200

    Képnézegető alapok

--- src/fajl/FajlNezegeto.java
+++ src/fajl/FajlNezegeto.java
@@ -3,18 +3,32 @@ package fajl;
 import fajl.kepFajlok.*;
 
 import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.io.File;
 
 public class FajlNezegeto {
     JFrame ablak = new JFrame();
 
-    public FajlNezegeto(File fajl) {
-        switch (fajl.getKiterjesztes()) {
-            case "jpg": new JPG(fajl);
-            break;
-            case "icon": new Ikon(fajl);
-        }
+    public FajlNezegeto(File file) {
+        final int[] x = {600};
+        final int[] y = {600};
+        ablak.add(JPG.megnyit(file, x[0], y[0]));
+        ablak.addComponentListener(new ComponentAdapter() {
+            public void componentResized(ComponentEvent e) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+                x[0] = ablak.getWidth();
+                y[0] = ablak.getHeight();
+                ablak.add(JPG.megnyit(file, x[0], y[0]));
 
+            }
+        });
+        init();
     }
 
     private void init() {
