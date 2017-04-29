911d27ade5faf0241230042efd45f758713f8c51
Borosh <aboros97@gmail.com>
2017-04-29 19:52:06 +0200

    Apró, főleg formai hibák javítva. Pár kommenttel bővült a kód.

--- src/fajl/Fajl.java
+++ src/fajl/Fajl.java
@@ -1,87 +1,86 @@
 package fajl;
 
-//import java.awt.Image;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import javax.swing.ImageIcon;
 
 public class Fajl {
-	private String nev;
-	private String kiterjesztes;
-	private long meret;
-	private String datum;
-	private ImageIcon ikon;
-	private int pont;
-	
-	public Fajl() {
-	}
-	
-	public void setFajl(String ut) {
-		File fajl = new File(ut);
-		nev = fajl.getName();
-		pont = nev.length();
-		for(int i=nev.length()-1; i>=0; i--) {
-			if(nev.charAt(i) == '.') {
-				pont = i;
-				break;
-			}
-		}
-		nev = nev.substring(0, pont);
-		kiterjesztes = kiterjesztesCsinal(fajl);
-		meret = fajl.length();
-		datum = new SimpleDateFormat("MM/dd/yyyy HH:mm").format(fajl.lastModified());
-		//ikon = ;
-		if(!(kiterjesztes.equals(" "))) {
-			System.out.println(nev + "		" + kiterjesztes + "	" + meret + "	" + datum);
-		}
-	}
-	
-	public String kiterjesztesCsinal(File fajl) {
-		ArrayList<String> tamogatott = new ArrayList<String>();
-		tamogatott.add("txt");
-		tamogatott.add("rtf");
-		tamogatott.add("jpg");
-		tamogatott.add("icon");
-		tamogatott.add("<DIR>");
-		String utvonal = fajl.getAbsolutePath();
-		String str = "";
-		//int pont = utvonal.length()-1;
-		for(int i=utvonal.length()-1; i>=0; i--) {
-			if(utvonal.charAt(i) == '.' && i != 0) {
-				pont = i;
-				break;
-			}
-		}
-		if(pont != utvonal.length()-1) {
-			for(int i=pont+1; i<utvonal.length(); i++) {
-				str += utvonal.charAt(i);
-			}
-		}
-		else {
-			str = "<DIR>";
-		}
-		if(tamogatott.contains(str)) {
-			return str;
-		}
-		else {
-			return " ";
-		}
-	}
-	
-	public String getNev() {
-		return nev;
-	}
-	
-	public String getKiterjesztes() {
-		return kiterjesztes;
-	}
-	
-	public long getMeret() {
-		return meret;
-	}
+    private String nev;
+    private String kiterjesztes;
+    private long meret;
+    private String datum;
+    private ImageIcon ikon;
+    private int pont;
 
-	public String getDatum() {
-		return datum;
-	}
+
+    public void setFajl(String utvonal) {
+        File fajl = new File(utvonal);
+        nev = fajl.getName();
+        pont = nev.length();
+        for (int i = nev.length() - 1; i >= 0; i--) {
+            if (nev.charAt(i) == '.') {
+                pont = i;
+                break;
+            }
+        }
+
+        /* Használhatnál splitet, ami egy tömböt ad vissza, a tömb ellenőrzésével
+        pedig le birod ellenőrizni a spec. kiterjesztéseket is, mint pl. a tar.gz
+         nev = nev.split(".")[0];
+         */
+        nev = nev.substring(0, pont);
+        kiterjesztes = kiterjesztesCsinal(fajl);
+        meret = fajl.length();
+        datum = new SimpleDateFormat("MM/dd/yyyy HH:mm").format(fajl.lastModified());
+        if (!(kiterjesztes.equals(' '))) {
+            System.out.println(nev + "		" + kiterjesztes + "	" + meret + "	" + datum);
+        }
+    }
+
+    private String kiterjesztesCsinal(File fajl) {
+        ArrayList<String> tamogatott = new ArrayList<>();
+        tamogatott.add("txt");
+        tamogatott.add("rtf");
+        tamogatott.add("jpg");
+        tamogatott.add("icon");
+        tamogatott.add("<DIR>");
+        String utvonal = fajl.getAbsolutePath();
+        String str = "";
+
+        for (int i = utvonal.length() - 1; i >= 0; i--) {
+            if (utvonal.charAt(i) == '.' && i != 0) {
+                pont = i;
+                break;
+            }
+        }
+        if (pont != utvonal.length() - 1) {
+            for (int i = pont + 1; i < utvonal.length(); i++) {
+                str += utvonal.charAt(i);
+            }
+        } else {
+            str = "<DIR>";
+        }
+        if (tamogatott.contains(str)) {
+            return str;
+        } else {
+            return " ";
+        }
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public String getKiterjesztes() {
+        return kiterjesztes;
+    }
+
+    public long getMeret() {
+        return meret;
+    }
+
+    public String getDatum() {
+        return datum;
+    }
 }
--- src/fajl/FajlLista.java
+++ src/fajl/FajlLista.java
@@ -1,18 +1,21 @@
 package fajl;
+
 import java.io.File;
 
 public class FajlLista {
-	public File file;
-	public File[] lista;
-	public Fajl[] fajl;
-	
-	public FajlLista(String utvonal) {
-		file = new File(utvonal);
-		lista = file.listFiles();
-		fajl = new Fajl[lista.length];
-		for(int i=0; i<lista.length; i++) {
-			fajl[i] = new Fajl();
-			fajl[i].setFajl(lista[i].getAbsolutePath());
-		}
-	}
+    private File file;
+    private File[] lista;
+    private Fajl[] fajl;
+
+    //getter a listához. Azt sok helyen le kell kérni
+    public FajlLista(String direktorium) {
+        file = new File(direktorium);
+        lista = file.listFiles();
+        // esetleges NullException kezelése
+        fajl = new Fajl[lista.length];
+        for (int i = 0; i < lista.length; i++) {
+            fajl[i] = new Fajl();
+            fajl[i].setFajl(lista[i].getAbsolutePath());
+        }
+    }
 }
