45de116c77e6b44dcbd58b026fedf132b258b9f9
Szkocsovszki Zsolt <szkocsovszki.zsolt@gmail.com>
2017-05-13 01:44:50 +0200

    Működő FajlLista

--- src/fajl/Fajl.java
+++ src/fajl/Fajl.java
@@ -2,70 +2,30 @@ package fajl;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import javax.swing.ImageIcon;
+//import javax.swing.ImageIcon;
 
 public class Fajl {
     private String nev;
     private String kiterjesztes;
-    private long meret;
+    private String meret;
     private String datum;
-    private ImageIcon ikon;
-    private int pont;
+    //private ImageIcon ikon;
 
 
     public void setFajl(String utvonal) {
         File fajl = new File(utvonal);
-        nev = fajl.getName();
-        pont = nev.length();
-        for (int i = nev.length() - 1; i >= 0; i--) {
-            if (nev.charAt(i) == '.') {
-                pont = i;
-                break;
-            }
+        Kiterjesztesek.tamogatottTipusok();
+        nev = Kiterjesztesek.nev(fajl);
+        kiterjesztes = Kiterjesztesek.kiterjesztes(fajl);
+        if(fajl.isDirectory()) {
+        	meret = "";
         }
-
-        /* Használhatnál splitet, ami egy tömböt ad vissza, a tömb ellenőrzésével
-        pedig le birod ellenőrizni a spec. kiterjesztéseket is, mint pl. a tar.gz
-         nev = nev.split(".")[0];
-         */
-        nev = nev.substring(0, pont);
-        kiterjesztes = kiterjesztesCsinal(fajl);
-        meret = fajl.length();
+        else {
+        	meret = Long.toString(fajl.length());
+        }
         datum = new SimpleDateFormat("MM/dd/yyyy HH:mm").format(fajl.lastModified());
-        if (!(kiterjesztes.equals(' '))) {
-            System.out.println(nev + "		" + kiterjesztes + "	" + meret + "	" + datum);
-        }
-    }
-
-    private String kiterjesztesCsinal(File fajl) {
-        ArrayList<String> tamogatott = new ArrayList<>();
-        tamogatott.add("txt");
-        tamogatott.add("rtf");
-        tamogatott.add("jpg");
-        tamogatott.add("icon");
-        tamogatott.add("<DIR>");
-        String utvonal = fajl.getAbsolutePath();
-        String str = "";
-
-        for (int i = utvonal.length() - 1; i >= 0; i--) {
-            if (utvonal.charAt(i) == '.' && i != 0) {
-                pont = i;
-                break;
-            }
-        }
-        if (pont != utvonal.length() - 1) {
-            for (int i = pont + 1; i < utvonal.length(); i++) {
-                str += utvonal.charAt(i);
-            }
-        } else {
-            str = "<DIR>";
-        }
-        if (tamogatott.contains(str)) {
-            return str;
-        } else {
-            return " ";
-        }
+ 
+        //System.out.println(nev + "		" + kiterjesztes + "	" + meret + "	" + datum);
     }
 
     public String getNev() {
@@ -76,7 +36,7 @@ public class Fajl {
         return kiterjesztes;
     }
 
-    public long getMeret() {
+    public String getMeret() {
         return meret;
     }
 
--- src/fajl/FajlLista.java
+++ src/fajl/FajlLista.java
@@ -7,15 +7,21 @@ public class FajlLista {
     private File[] lista;
     private Fajl[] fajl;
 
-    //getter a listához. Azt sok helyen le kell kérni
     public FajlLista(String direktorium) {
         file = new File(direktorium);
         lista = file.listFiles();
-        // esetleges NullException kezelése
-        fajl = new Fajl[lista.length];
-        for (int i = 0; i < lista.length; i++) {
-            fajl[i] = new Fajl();
-            fajl[i].setFajl(lista[i].getAbsolutePath());
+        try {
+        	fajl = new Fajl[lista.length];
+        	for (int i = 0; i < lista.length; i++) {
+        		fajl[i] = new Fajl();
+        		fajl[i].setFajl(lista[i].getAbsolutePath());
+        	}
         }
+        catch(NullPointerException e) {
+        }
+    }
+    
+    public Fajl[] getFajlLista() {
+    	return fajl;
     }
 }
