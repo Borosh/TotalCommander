d0112aa3a71815858d05c6155d1a06e4e381e22f
Borosh <aboros97@gmail.com>
2017-05-13 01:34:43 +0200

    Fajlnezegeto majdnem vegleges

--- src/fajl/FajlNezegeto.java
+++ src/fajl/FajlNezegeto.java
@@ -1,30 +1,33 @@
 package fajl;
 
 import fajl.kepFajlok.*;
-import fajl.szovegesFajlok.*;
+
 import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class FajlNezegeto {
+    static JFrame ablak = new JFrame();
 
     public FajlNezegeto(String file, String kiterjesztes) {
-    	JFrame ablak = new JFrame();
+        JFrame ablak = new JFrame();
         JPanel fajl = null;
-    	ablak.setBounds(500,200,600,600);
-        ablak.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        ablak.setBounds(500, 200, 600, 600);
+        ablak.setVisible(true);
         int x = ablak.getContentPane().getWidth();
         int y = ablak.getContentPane().getHeight();
-        
+
         if (kiterjesztes.equals("rtf")) {
-        	fajl = new Rtf(file, x, y);
-        }else if (kiterjesztes.equals("jpg")) {
+            //  fajl = new Rtf(file, x, y);
+        } else if (kiterjesztes.equals("jpg")) {
             fajl = new JPG(file, x, y);
-        }else if (kiterjesztes.equals("ico")) {
+        } else if (kiterjesztes.equals("ico")) {
             fajl = new Ikon.megnyit(file, x, y);
         }
-        
-        System.out.println(fajl);
-        ablak.setLayout(null);
+
         ablak.add(fajl);
         ablak.setVisible(true);
+        ablak.setResizable(false);
+        ablak.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
