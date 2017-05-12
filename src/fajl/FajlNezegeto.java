f9baa9417b3142dbd377fd4a24816d5366359cb4
Borosh <aboros97@gmail.com>
2017-05-12 23:24:55 +0200

    Fajlnezegeto beta

--- src/fajl/FajlNezegeto.java
+++ src/fajl/FajlNezegeto.java
@@ -7,14 +7,27 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.io.File;
+import java.io.IOException;
 
 public class FajlNezegeto {
-    JFrame ablak = new JFrame();
+    static JFrame ablak = new JFrame();
 
-    public FajlNezegeto(File file) {
+    public FajlNezegeto() {
+
+    }
+
+    public static boolean megnyit(String file) {
         final int[] x = {600};
         final int[] y = {600};
-        ablak.add(JPG.megnyit(file, x[0], y[0]));
+
+        JPanel fajl = null;
+        if (file.contains(".jpg")) {
+            fajl = new JPG.megnyit(file, x[0], y[0]);
+        } else if (file.contains(".ico")) {
+            fajl = new Ikon.megnyit(file, x[0], y[0]);
+        }
+
+        ablak.add(fajl);
         ablak.addComponentListener(new ComponentAdapter() {
             public void componentResized(ComponentEvent e) {
                 try {
@@ -24,14 +37,15 @@ public class FajlNezegeto {
                 }
                 x[0] = ablak.getWidth();
                 y[0] = ablak.getHeight();
-                ablak.add(JPG.megnyit(file, x[0], y[0]));
+                ablak.add(new JPG.megnyit(file, x[0], y[0]));
 
             }
         });
         init();
+        return true;
     }
 
-    private void init() {
+    private static void init() {
         ablak.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         ablak.setSize(ablak.getPreferredSize());
         ablak.setVisible(true);
