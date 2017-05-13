b4688802346097fb0445646c37a182744f5b7ef7
Szkocsovszki Zsolt <szkocsovszki.zsolt@gmail.com>
2017-05-13 03:35:59 +0200

    merge commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import fajl.FajlNezegeto;
-
+import fajl.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        FajlNezegeto fajlNezegeto = new FajlNezegeto("C:\\Users\\Boros\\Downloads\\17857667_1862388513786755_748469787_n (2).jpg", "rtf");
     }
 }
