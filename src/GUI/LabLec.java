40f675d9fff1a49cbbf4d4781f176442a0bd0529
unknown <TerhesBalazs>
2017-05-24 16:36:18 +0200

    Hibakezelés helyes kezelései

--- src/GUI/LabLec.java
+++ src/GUI/LabLec.java
@@ -9,112 +9,110 @@ import java.nio.file.Paths;
 
 class LabLec extends JPanel {
 
-    static JButton F3 = new JButton("F3 Nézőke");
-    static JButton F5 = new JButton("F5 Másolás ");
-    static JButton F6 = new JButton("F6 Áthelyez");
-    static JButton F7 = new JButton("F7 Új mappa");
-    static JButton F8 = new JButton("F8 Törlés");
-    static JButton F1 = new JButton("F1 Ablak váltás");
+	static JButton F3 = new JButton("F3 Nézőke");
+	static JButton F5 = new JButton("F5 Másolás ");
+	static JButton F6 = new JButton("F6 Áthelyez");
+	static JButton F7 = new JButton("F7 Új mappa");
+	static JButton F8 = new JButton("F8 Törlés");
+	static JButton F1 = new JButton("F1 Ablak váltás");
 
-    LabLec() {
-        setLayout(new GridLayout(1, 5));
-        setMinimumSize(new Dimension(400, 50));
+	LabLec() {
+		setLayout(new GridLayout(1, 5));
+		setMinimumSize(new Dimension(400, 50));
 
-        add(F1);
-        add(F3);
-        add(F5);
-        add(F6);
-        add(F7);
-        add(F8);
+		add(F1);
+		add(F3);
+		add(F5);
+		add(F6);
+		add(F7);
+		add(F8);
 
-        F3.addActionListener(e -> {
+		F1.addActionListener(e -> {
+			Ablak.fokuszValtas();
+			Ablak.fokuszbanVan.fajlLista.frissit(Ablak.fokuszbanVan.fajlLista.helyzet);
+			Ablak.nincsFokuszban.fajlLista.frissit(Ablak.nincsFokuszban.fajlLista.helyzet);
+			for (JList<String> i : Ablak.fokuszbanVan.lista)
+				i.setSelectedIndex(0);
+			Ablak.fokuszbanVan.fajlok.transferFocus();
+		});
 
-            try {
-                Navigacio.interakcio((Ablak.fokuszbanVan.fajlLista.lista
-                        .get(Ablak.fokuszbanVan.lista.get(0).getSelectedIndex())));
-            } catch (Exception e1) {
-            }
-            Ablak.fokuszbanVan.fajlLista.frissit(Ablak.fokuszbanVan.fajlLista.helyzet);
-            Ablak.nincsFokuszban.fajlLista.frissit(Ablak.nincsFokuszban.fajlLista.helyzet);
+		F3.addActionListener(e -> {
 
-            Ablak.fokuszbanVan.fajlok.transferFocus();
-        });
+			try {
+				Navigacio.interakcio(
+						(Ablak.fokuszbanVan.fajlLista.lista.get(Ablak.fokuszbanVan.lista.get(0).getSelectedIndex())));
+			} catch (Exception e1) {
+			}
+			Ablak.fokuszbanVan.fajlLista.frissit(Ablak.fokuszbanVan.fajlLista.helyzet);
+			Ablak.nincsFokuszban.fajlLista.frissit(Ablak.nincsFokuszban.fajlLista.helyzet);
 
-        F5.addActionListener(e -> {
-            try {
-                Operaciok.masol(
-                        Paths.get(Ablak.fokuszbanVan.fajlLista.lista
-                                .get(Ablak.fokuszbanVan.lista.get(0).getSelectedIndex()).getAbsolutePath()),
-                        Paths.get(Ablak.nincsFokuszban.fajlLista.helyzet.getAbsolutePath()));
-                Ablak.fokuszbanVan.fajlLista.frissit(Ablak.fokuszbanVan.fajlLista.helyzet);
-                Ablak.nincsFokuszban.fajlLista.frissit(Ablak.nincsFokuszban.fajlLista.helyzet);
+			Ablak.fokuszbanVan.fajlok.transferFocus();
+		});
 
-                Ablak.fokuszbanVan.fajlok.transferFocus();
-            } catch (Exception e1) {
-                JOptionPane.showMessageDialog(null, "File másolása közbeni hiba!", "Hiba",
-                        JOptionPane.ERROR_MESSAGE);
-            }
-        });
+		F5.addActionListener(e -> {
+			try {
+				Operaciok.masol(
+						Paths.get(Ablak.fokuszbanVan.fajlLista.lista
+								.get(Ablak.fokuszbanVan.lista.get(0).getSelectedIndex()).getAbsolutePath()),
+						Paths.get(Ablak.nincsFokuszban.fajlLista.helyzet.getAbsolutePath()));
+				Ablak.fokuszbanVan.fajlLista.frissit(Ablak.fokuszbanVan.fajlLista.helyzet);
+				Ablak.nincsFokuszban.fajlLista.frissit(Ablak.nincsFokuszban.fajlLista.helyzet);
 
-        F6.addActionListener(e -> {
-            try {
-                Operaciok.athelyez(
-                        Paths.get(Ablak.fokuszbanVan.fajlLista.lista
-                                .get(Ablak.fokuszbanVan.lista.get(0).getSelectedIndex()).getAbsolutePath()),
-                        Paths.get(Ablak.nincsFokuszban.fajlLista.helyzet.getAbsolutePath()));
-                Ablak.fokuszbanVan.fajlLista.frissit(Ablak.fokuszbanVan.fajlLista.helyzet);
-                Ablak.nincsFokuszban.fajlLista.frissit(Ablak.nincsFokuszban.fajlLista.helyzet);
+				Ablak.fokuszbanVan.fajlok.transferFocus();
+			} catch (Exception e1) {
+				JOptionPane.showMessageDialog(null, e1.getMessage(), "Hiba", JOptionPane.ERROR_MESSAGE);
+			}
+		});
 
-                Ablak.fokuszbanVan.fajlok.transferFocus();
-            } catch (Exception e1) {
-                JOptionPane.showMessageDialog(null, "File áthelyez közbeni hiba!", "Hiba",
-                        JOptionPane.ERROR_MESSAGE);
-            }
-        });
+		F6.addActionListener(e -> {
+			try {
+				Operaciok.athelyez(
+						Paths.get(Ablak.fokuszbanVan.fajlLista.lista
+								.get(Ablak.fokuszbanVan.lista.get(0).getSelectedIndex()).getAbsolutePath()),
+						Paths.get(Ablak.nincsFokuszban.fajlLista.helyzet.getAbsolutePath()));
+				Ablak.fokuszbanVan.fajlLista.frissit(Ablak.fokuszbanVan.fajlLista.helyzet);
+				Ablak.nincsFokuszban.fajlLista.frissit(Ablak.nincsFokuszban.fajlLista.helyzet);
 
-        F8.addActionListener(e -> {
-            try {
-                int hibaUzenet = JOptionPane.showConfirmDialog(null,
-                        "Biztos ki szeretné törölni a \"" + Ablak.fokuszbanVan.fajlLista.lista
-                                .get(Ablak.fokuszbanVan.lista.get(0).getSelectedIndex()).getName() + "\" nevű elemet?",
-                        "Törlés", JOptionPane.YES_NO_OPTION);
-                if (hibaUzenet == JOptionPane.YES_OPTION) {
-                    Operaciok.torol(Paths.get(Ablak.fokuszbanVan.fajlLista.lista
-                            .get(Ablak.fokuszbanVan.lista.get(0).getSelectedIndex()).getAbsolutePath()));
-                    Ablak.fokuszbanVan.fajlLista.frissit(Ablak.fokuszbanVan.fajlLista.helyzet);
-                    Ablak.nincsFokuszban.fajlLista.frissit(Ablak.nincsFokuszban.fajlLista.helyzet);
+				Ablak.fokuszbanVan.fajlok.transferFocus();
+			} catch (Exception e1) {
+				JOptionPane.showMessageDialog(null, e1.getMessage(), "Hiba", JOptionPane.ERROR_MESSAGE);
+			}
+		});
 
-                    Ablak.fokuszbanVan.fajlok.transferFocus();
-                }
-            } catch (Exception e1) {
-                JOptionPane.showMessageDialog(null, "File törlés közbeni hiba!", "Hiba", JOptionPane.ERROR_MESSAGE);
-            }
-        });
+		F7.addActionListener(e -> {
+			try {
+				String nev = JOptionPane.showInputDialog(null, "Az új mappa neve", "Új mappa",
+						JOptionPane.QUESTION_MESSAGE);
+				if (nev != null) {
+					Operaciok.ujMappa(Paths.get(Ablak.fokuszbanVan.fajlLista.helyzet.getAbsolutePath() + "\\" + nev));
+					Ablak.fokuszbanVan.fajlLista.frissit(Ablak.fokuszbanVan.fajlLista.helyzet);
+					Ablak.nincsFokuszban.fajlLista.frissit(Ablak.nincsFokuszban.fajlLista.helyzet);
+					Ablak.fokuszbanVan.fajlok.transferFocus();
+				}
+			} catch (Exception e1) {
+				JOptionPane.showMessageDialog(null, e1.getMessage(), "Hiba", JOptionPane.ERROR_MESSAGE);
+			}
+		});
 
-        F7.addActionListener(e -> {
-            try {
-                String nev = JOptionPane.showInputDialog(null, "Az új mappa neve", "Új mappa", JOptionPane.QUESTION_MESSAGE);
-                if (nev != null) {
-                    Operaciok.ujMappa(
-                            Paths.get(Ablak.fokuszbanVan.fajlLista.helyzet.getAbsolutePath() + "\\" + nev));
-                    Ablak.fokuszbanVan.fajlLista.frissit(Ablak.fokuszbanVan.fajlLista.helyzet);
-                    Ablak.nincsFokuszban.fajlLista.frissit(Ablak.nincsFokuszban.fajlLista.helyzet);
-                    Ablak.fokuszbanVan.fajlok.transferFocus();
-                }
-            } catch (Exception e1) {
-                JOptionPane.showMessageDialog(null, "Új mappa létrhezása közbeni hiba!", "Hiba",
-                        JOptionPane.ERROR_MESSAGE);
-            }
-        });
+		F8.addActionListener(e -> {
+			try {
+				int hibaUzenet = JOptionPane.showConfirmDialog(null,
+						"Biztos ki szeretné törölni a \""
+								+ Ablak.fokuszbanVan.fajlLista.lista
+										.get(Ablak.fokuszbanVan.lista.get(0).getSelectedIndex()).getName()
+								+ "\" nevű elemet?",
+						"Törlés", JOptionPane.YES_NO_OPTION);
+				if (hibaUzenet == JOptionPane.YES_OPTION) {
+					Operaciok.torol(Paths.get(Ablak.fokuszbanVan.fajlLista.lista
+							.get(Ablak.fokuszbanVan.lista.get(0).getSelectedIndex()).getAbsolutePath()));
+					Ablak.fokuszbanVan.fajlLista.frissit(Ablak.fokuszbanVan.fajlLista.helyzet);
+					Ablak.nincsFokuszban.fajlLista.frissit(Ablak.nincsFokuszban.fajlLista.helyzet);
 
-        F1.addActionListener(e -> {
-            Ablak.fokuszValtas();
-            Ablak.fokuszbanVan.fajlLista.frissit(Ablak.fokuszbanVan.fajlLista.helyzet);
-            Ablak.nincsFokuszban.fajlLista.frissit(Ablak.nincsFokuszban.fajlLista.helyzet);
-            for (JList<String> i : Ablak.fokuszbanVan.lista)
-                i.setSelectedIndex(0);
-            Ablak.fokuszbanVan.fajlok.transferFocus();
-        });
-    }
-
+					Ablak.fokuszbanVan.fajlok.transferFocus();
+				}
+			} catch (Exception e1) {
+				JOptionPane.showMessageDialog(null, e1.getMessage(), "Hiba", JOptionPane.ERROR_MESSAGE);
+			}
+		});
+	}
 }
--- src/fajl/Operaciok.java
+++ src/fajl/Operaciok.java
@@ -8,55 +8,68 @@ import java.nio.file.Paths;
 
 public class Operaciok {
 
-    public static void masol(Path mit, Path hova) throws Exception {
-        try {
-            if (Files.isDirectory(mit)) {
-                new File(hova.toString() + "\\" + mit.getFileName()).mkdir();
-                for (String file : new File(mit.toString()).list()) {
-                    masol(Paths.get(mit.toString() + "\\" + file),
-                            Paths.get(hova.toString() + "\\" + mit.getFileName()));
-                }
-            } else {
-                Files.copy(mit, hova.resolve(mit.getFileName()));
-            }
-        } catch (Exception e) {
-            throw new IOException("Hiba a másolás során!");
-        }
-    }
+	public static void masol(Path mit, Path hova) throws Exception {
+		try {
+			if (Files.isDirectory(mit)) {
+				new File(hova.toString() + "\\" + mit.getFileName()).mkdir();
+				for (String file : new File(mit.toString()).list()) {
+					masol(Paths.get(mit.toString() + "\\" + file),
+							Paths.get(hova.toString() + "\\" + mit.getFileName()));
+				}
+			} else {
+				Files.copy(mit, hova.resolve(mit.getFileName()));
+			}
+		} catch (Exception e) {
+			throw new IOException("Hiba a másolás során!");
+		}
+	}
 
-    public static void torol(Path mit) throws Exception {
-        try {
-            if (Files.isDirectory(mit)) {
-                for (String fajl : new File(mit.toString()).list()) {
-                    torol(Paths.get(mit + "\\" + fajl));
-                }
-                Files.delete(mit);
-            } else if (Files.exists(mit)) {
-                Files.delete(mit);
-            } else
-                throw new Exception("Nem létező fájlt nem lehet törölni!");
-        } catch (Exception e) {
-            if (e.getMessage().equals("Nem létező fájlt nem lehet törölni!")) {
-                throw new IOException("Nem létező fájlt nem lehet törölni!");
-            }
-            throw new IOException(e.getMessage());
-        }
-    }
+	public static void torol(Path mit) throws Exception {
+		try {
+			if (Files.isDirectory(mit)) {
+				for (String fajl : new File(mit.toString()).list()) {
+					torol(Paths.get(mit + "\\" + fajl));
+				}
+				try {
+					Files.delete(mit);
+				} catch (Exception e1) {
+					throw new Exception("Hiba a fájl törlése közben!");
+				}
+			} else if (Files.exists(mit)) {
+				try {
+					Files.delete(mit);
+				} catch (Exception e1) {
+					throw new Exception("Hiba a fájl törlése közben!");
+				}
+			} else
+				throw new Exception("Nem létező fájlt nem lehet törölni!");
+		} catch (Exception e) {
+			throw new IOException(e.getMessage());
+		}
+	}
 
-    public static void athelyez(Path mit, Path hova) throws Exception {
-        try {
-            masol(mit, hova);
-            torol(mit);
-        } catch (Exception e) {
-            throw new Exception("Hiba az áthelyezés közben!");
-        }
-    }
+	public static void athelyez(Path mit, Path hova) throws Exception {
+		try {
+			masol(mit, hova);
+			torol(mit);
+		} catch (Exception e) {
+			throw new Exception("Hiba az áthelyezés közben!");
+		}
+	}
 
-    public static void ujMappa(Path nev) throws Exception {
-        if (!(new File(nev.toString()).exists())) {
-            (new File(nev.toString())).mkdirs();
-        } else {
-            throw new IOException("Ez a mappa már létezik");
-        }
-    }
+	public static void ujMappa(Path nev) throws Exception {
+		try {
+			if (!(new File(nev.toString()).exists())) {
+				try {
+					(new File(nev.toString())).mkdirs();
+				} catch (Exception e) {
+					throw new IOException("Hiba a új mappa létrehozásako!");
+				}
+			} else {
+				throw new IOException("Ez a mappa már létezik");
+			}
+		} catch (Exception e1) {
+			throw new IOException(e1.getMessage());
+		}
+	}
 }
